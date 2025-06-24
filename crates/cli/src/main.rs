eb2b017c5dab47b3edae60b27d3c4cb66b7c44da
gasin <7404299+gasin@users.noreply.github.com>
2025-06-25 01:30:52 +0900

    add heurs command

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,18 +1,31 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 struct Cli {
-    pattern: String,
-    path: std::path::PathBuf,
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Run {
+        pattern: String,
+        path: std::path::PathBuf,
+    },
 }
 
 fn main() {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(args.path).expect("Should have been able to read the file");
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+    match &args.command {
+        Commands::Run { pattern, path } => {
+            let content =
+                std::fs::read_to_string(path).expect("Should have been able to read the file");
+            for line in content.lines() {
+                if line.contains(pattern) {
+                    println!("{}", line);
+                }
+            }
         }
     }
 }
